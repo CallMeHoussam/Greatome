d3565c051a59d22c123bab2c8131702a43f86ae8
Evoless <amoogames519@gmail.com>
2025-06-10 19:03:16 +0100

    zozo

---
+++ Ultimate.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+void ft_ultimate_ft(int *********nbr)
+{
+    nbr = 42;
+    int *nbr1 = &nbr;
+    int **nbr2 = &nbr1;
+    int ***nbr3 = &nbr2;
+    int ****nbr4
+
+}
+int main(
+printf("%d",nbr1);
+)
